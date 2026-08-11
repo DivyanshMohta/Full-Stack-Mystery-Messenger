d4caaa58b21b2bcbcb7a1fe49ea3afb912c81b6a
DivyanshMohta <divyanshmohta021@gmail.com>
2026-08-11 18:59:53 +0530

    Added API endpoints for accepting and sending messages

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,52 @@
+import dbConnect from "@/lib/dbConnect";
+import { Message } from "@/model/User";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const { username, content } = await request.json();
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found ",
+        },
+        { status: 404 },
+      );
+    }
+    if (!user.isAcceptingMessage) {
+      return Response.json(
+        {
+          success: false,
+          message: "User is not accepting messages",
+        },
+        { status: 403 },
+      );
+    }
+
+    const newMessage = { content, createdAt: new Date() };
+
+    user.messages.push(newMessage as Message);
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message send Successfully",
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error(error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error in sending messages",
+      },
+      { status: 500 },
+    );
+  }
+}
