bacea239de31da453643b225b482a7ab4cd12aa8
DivyanshMohta <divyanshmohta021@gmail.com>
2026-08-19 13:06:28 +0530

    Fixed Layout files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toast";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +26,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <AuthProvider>
+        <body className="min-h-full flex flex-col">
+          <Navbar />
+          {children}
+          <Toaster />
+        </body>
+      </AuthProvider>
     </html>
   );
 }
