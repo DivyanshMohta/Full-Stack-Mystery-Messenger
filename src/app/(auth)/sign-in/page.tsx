fb76ca8f27edb19de09560152cb4096d16656e21
DivyanshMohta <divyanshmohta021@gmail.com>
2026-08-19 13:08:13 +0530

    Added Dashboard page, fixed sign up redirection error

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -104,10 +104,7 @@ const SignIn = () => {
         <div className="text-center mt-4">
           <p>
             Not a member?{" "}
-            <Link
-              href="/api/sign-up"
-              className="text-blue-600 hover:text-blue-800"
-            >
+            <Link href="sign-up" className="text-blue-600 hover:text-blue-800">
               Sign Up
             </Link>
           </p>
